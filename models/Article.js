fee81e8c1783d0c2c4b4c276f5f37489154dd069
Luke Klymshyn <unseenmountain@gmail.com>
2020-03-27 17:45:16 -0700

    remaking heroku app

--- models/Article.js
+++ models/Article.js
@@ -1,33 +1,34 @@
 var mongoose = require("mongoose");
 
+
 var Schema = mongoose.Schema;
 
-var ArticleSchema = new Schema({
-
-    headLine: {
+var ArticleSchema = new Schema ({
+    headline: {
         type: String,
         required: true
     },
-
-    summery: {
+    summaryOne: {
         type: String,
         required: true
     },
-
+    summaryTwo: {
+        type: String,
+        required: false
+    },
+    link: {
+        type: String,
+        required: true
+    },
     saved: {
         type: Boolean,
-       default: false
+        default: false
     },
-    // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  Note: [{
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }]
+    note: 
+        []
+    
 });
 
-var Article = mongoose.module("Article", ArticleSchema);
+var Article = mongoose.model("Article", ArticleSchema);
 
 module.exports = Article;
-
--- models/Note.js
+++ models/Note.js
@@ -2,18 +2,11 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var noteSchema = new Schema({
-
-  _headlineId: {
-    type: Schema.Types.ObjectId,
-    ref: "Headline"
-  },
-
-  date: String,
-
-  noteText: String
+var NoteSchema = new Schema({
+    title: String,
+    body: String
 });
 
-var Note = mongoose.model("Note", noteSchema);
+var Note = mongoose.model("Note", NoteSchema);
 
 module.exports = Note;
